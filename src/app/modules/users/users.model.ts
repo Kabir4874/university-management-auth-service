90ff4bb2f05f4bee6c3c7b1c0e38ac39992f6173
Kabir Ahmed <kabir@Kabirs-Mac-mini.local>
2025-11-10 20:40:07 +0600

    [Done] user interface and model created

---
+++ src/app/modules/users/users.model.ts
@@ -0,0 +1,27 @@
+import { model, Model, Schema } from 'mongoose';
+import type { IUser } from './users.interface.js';
+
+type UserModel = Model<IUser, object>;
+
+const userSchema = new Schema<IUser>(
+  {
+    id: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    role: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true },
+);
+
+const User = model<IUser, UserModel>('User', userSchema);
+
+export default User;
