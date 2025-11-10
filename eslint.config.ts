79f38debf4ea7cbe526fa48d8c318835c038e8c4
Kabir Ahmed <kabir@Kabirs-Mac-mini.local>
2025-11-10 16:27:08 +0600

    [Done] setup husky

---
+++ eslint.config.ts
@@ -0,0 +1,32 @@
+import js from '@eslint/js';
+import prettierConfig from 'eslint-config-prettier';
+import prettierPlugin from 'eslint-plugin-prettier';
+import { defineConfig } from 'eslint/config';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+export default defineConfig([
+  { ignores: ['node_modules/', 'dist/', 'build/', '.env'] },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  prettierConfig,
+  {
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
+    plugins: {
+      prettier: prettierPlugin,
+    },
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: process.cwd(),
+      },
+    },
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'error',
+      'no-unused-vars': 'warn',
+    },
+  },
+]);
